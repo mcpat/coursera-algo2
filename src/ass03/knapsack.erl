402347707b5f78506c92d689efcdde0bf205f771
Marcel Patzlaff <marcel.patzlaff@dai-labor.de>
2013-10-23 00:48:50 +0200

    CHANGE: also keep reconstruction information

--- src/ass03/knapsack.erl
+++ src/ass03/knapsack.erl
@@ -8,10 +8,10 @@
 
 load_problem(Name) ->
     {Ok,Device} = file:open(string:concat("ass03/",Name), [read]),
-    {Ok,[W, _NumItems]} = io:fread(Device, "", "~d ~d"),
+    {Ok,[W, NumItems]} = io:fread(Device, "", "~d ~d"),
     try
         Items=get_all_items(Device),
-        {W, Items}
+        {W, Items, NumItems}
         after file:close(Device)
     end.
 
@@ -23,41 +23,60 @@ get_all_items(Device) ->
     end.
 
 
-knapsack({Size, Items}) ->
-    Result=itemloop(Size, Items, [{0,0}]),
-    {_Idx, Value}= lists:last(Result),
-    Value.
-
-itemloop(_, [], A) -> A;
-itemloop(Size, [#item{value=V,weight=W} | Rest], A) ->
-    NewA= xloop(W, V, Size, A, A),
-    itemloop(Size, Rest, NewA).
+knapsack({Size, Items, NumItems}) ->
+    Result=itemloop(1, Size, Items, [{0,0,[]}]),
+    reconstruct(lists:last(Result), lists:reverse(Items), NumItems).
 
 
-xloop(_W, _V, _Max, Copy, []) -> Copy;
+reconstruct({_,_,Causes}, Items, NumItems) ->
+    reconstruct(Causes, Items, NumItems);
 
-xloop(W, _V, Max, Copy, [{LIdx,_} | _]) when LIdx + W > Max -> Copy;
+reconstruct([], _, _) -> [];
 
-xloop(W, V, Max, [], [{LIdx, LVal} | LRest]) ->
-    [{LIdx + W, LVal + V} | xloop(W, V, Max, [], LRest)];
-
-
-xloop(W, V, Max, Copy=[{CIdx, CVal} | _], [{LIdx, LVal} | LRest]) when (CIdx =< LIdx + W) and (CVal >= LVal + V) ->
-    xloop(W, V, Max, Copy, LRest);
-
-xloop(W, V, Max, [{CIdx, CVal} | CRest], Lookup=[{LIdx, LVal} | _]) when (CIdx >= LIdx + W) and (CVal =< LVal + V) ->
-    xloop(W, V, Max, CRest, Lookup);
-
-xloop(W, V, Max, [CF={CIdx, _} | CRest], Lookup=[{LIdx, _} | _]) when CIdx < LIdx + W ->
-    [CF | xloop(W, V, Max, CRest, Lookup)];
+reconstruct(C=[CIdx | _], [_ | Rest], ItemIndex) when ItemIndex > CIdx ->
+    reconstruct(C, Rest, ItemIndex - 1);
 
 
-xloop(W, V, Max, [{CIdx, CVal} | CRest], [{LIdx, LVal} | LRest]) when (CIdx =:= LIdx + W) ->
-    [{CIdx, erlang:max(CVal, LVal + V)} | xloop(W, V, Max, CRest, LRest)];
+reconstruct([ItemIndex | CRest], [First | IRest], ItemIndex) ->
+    [First | reconstruct(CRest, IRest, ItemIndex - 1)].
 
 
-xloop(W, V, Max, Copy, [{LIdx, LVal} | LRest]) ->
-    [{LIdx + W, LVal + V} | xloop(W, V, Max, Copy, LRest)].
+itemloop(_I, _Max, [], A) -> A;
+itemloop(I, Max, [#item{value=V,weight=W} | Rest], A) ->
+    NewA= xloop(I, W, V, Max, A, A),
+    itemloop(I+1, Max, Rest, NewA).
+
+
+xloop(_I, _W, _V, _Max, Copy, []) -> Copy;
+
+xloop(_I, W, _V, Max, Copy, [{LIdx,_,_} | _]) when LIdx + W > Max -> Copy;
+
+xloop(I, W, V, Max, [], [{LIdx, LVal, Causes} | LRest]) ->
+    [{LIdx + W, LVal + V, [I | Causes]} | xloop(I, W, V, Max, [], LRest)];
+
+
+xloop(I, W, V, Max, Copy=[{CIdx, CVal, _} | _], [{LIdx, LVal, _} | LRest]) when (CIdx =< LIdx + W) and (CVal >= LVal + V) ->
+    xloop(I, W, V, Max, Copy, LRest);
+
+xloop(I, W, V, Max, [{CIdx, CVal, _} | CRest], Lookup=[{LIdx, LVal, _} | _]) when (CIdx >= LIdx + W) and (CVal =< LVal + V) ->
+    xloop(I, W, V, Max, CRest, Lookup);
+
+xloop(I, W, V, Max, [CF={CIdx, _, _} | CRest], Lookup=[{LIdx, _, _} | _]) when CIdx < LIdx + W ->
+    [CF | xloop(I, W, V, Max, CRest, Lookup)];
+
+
+xloop(I, W, V, Max, [CF={CIdx, CVal, _} | CRest], [{LIdx, LVal, Causes} | LRest]) when (CIdx =:= LIdx + W) ->
+    NewVal= LVal + V,
+    if
+        NewVal > CVal -> Front={CIdx, NewVal, [I | Causes]};
+        true -> Front=CF
+    end,
+    
+    [Front | xloop(I, W, V, Max, CRest, LRest)];
+
+
+xloop(I, W, V, Max, Copy, [{LIdx, LVal, Causes} | LRest]) ->
+    [{LIdx + W, LVal + V, [I | Causes]} | xloop(I, W, V, Max, Copy, LRest)].
     
     
 q1() ->
@@ -70,5 +89,5 @@ q2() ->
     knapsack(Problem).
 
 test() ->
-    Problem={6, [#item{value=3,weight=4}, #item{value=2,weight=3},#item{value=4,weight=2},#item{value=4,weight=3}]},
+    Problem={6, [#item{value=3,weight=4}, #item{value=2,weight=3},#item{value=4,weight=2},#item{value=4,weight=3}], 4},
     knapsack(Problem).
