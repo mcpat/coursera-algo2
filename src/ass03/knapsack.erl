49c94849830b2f5cc0e84b47288d1f9cf689dca3
Marcel Patzlaff <marcel.patzlaff@dai-labor.de>
2013-10-23 11:09:03 +0200

    CHANGE: added comments for better understanding

--- src/ass03/knapsack.erl
+++ src/ass03/knapsack.erl
@@ -25,46 +25,47 @@ get_all_items(Device) ->
 
 knapsack({Size, Items, NumItems}) ->
     Result=itemloop(1, Size, Items, [{0,0,[]}]),
-    reconstruct(lists:last(Result), lists:reverse(Items), NumItems).
+    {_,Value,Causes}= lists:last(Result),
+    {Value, reconstruct(Causes, lists:reverse(Items), NumItems)}.
 
-
-reconstruct({_,_,Causes}, Items, NumItems) ->
-    reconstruct(Causes, Items, NumItems);
 
 reconstruct([], _, _) -> [];
 
 reconstruct(C=[CIdx | _], [_ | Rest], ItemIndex) when ItemIndex > CIdx ->
     reconstruct(C, Rest, ItemIndex - 1);
 
-
 reconstruct([ItemIndex | CRest], [First | IRest], ItemIndex) ->
     [First | reconstruct(CRest, IRest, ItemIndex - 1)].
 
 
+% iterate through all items
 itemloop(_I, _Max, [], A) -> A;
 itemloop(I, Max, [#item{value=V,weight=W} | Rest], A) ->
     NewA= xloop(I, W, V, Max, A, A),
     itemloop(I+1, Max, Rest, NewA).
 
 
-xloop(_I, _W, _V, _Max, Copy, []) -> Copy;
+% iterate through all *necessary* weights of the knapsack
+% recursion anchor when weight lists are empty
+xloop(_I, _W, _V, _Max, [], []) -> [];
 
+% throw away combinations with the current item exceeding the maximum weight of the knapsack
 xloop(_I, W, _V, Max, Copy, [{LIdx,_,_} | _]) when LIdx + W > Max -> Copy;
 
-xloop(I, W, V, Max, [], [{LIdx, LVal, Causes} | LRest]) ->
-    [{LIdx + W, LVal + V, [I | Causes]} | xloop(I, W, V, Max, [], LRest)];
-
-
+% the following two rules ensure, that the resulting weight list is only as large as required
+% ignore combinations with the current item that are inferior (or equal) to those already known
 xloop(I, W, V, Max, Copy=[{CIdx, CVal, _} | _], [{LIdx, LVal, _} | LRest]) when (CIdx =< LIdx + W) and (CVal >= LVal + V) ->
     xloop(I, W, V, Max, Copy, LRest);
 
+% drop already known combinations that are inferior (or equal) to those with the current item
 xloop(I, W, V, Max, [{CIdx, CVal, _} | CRest], Lookup=[{LIdx, LVal, _} | _]) when (CIdx >= LIdx + W) and (CVal =< LVal + V) ->
     xloop(I, W, V, Max, CRest, Lookup);
 
+% keep already know combinations with lower weights 
 xloop(I, W, V, Max, [CF={CIdx, _, _} | CRest], Lookup=[{LIdx, _, _} | _]) when CIdx < LIdx + W ->
     [CF | xloop(I, W, V, Max, CRest, Lookup)];
 
-
+% keep the combination with higher value for the same weight
 xloop(I, W, V, Max, [CF={CIdx, CVal, _} | CRest], [{LIdx, LVal, Causes} | LRest]) when (CIdx =:= LIdx + W) ->
     NewVal= LVal + V,
     if
@@ -74,7 +75,7 @@ xloop(I, W, V, Max, [CF={CIdx, CVal, _} | CRest], [{LIdx, LVal, Causes} | LRest]
     
     [Front | xloop(I, W, V, Max, CRest, LRest)];
 
-
+% store new combinations with current item (CIdx > LIdx + W or Copy=:=[])
 xloop(I, W, V, Max, Copy, [{LIdx, LVal, Causes} | LRest]) ->
     [{LIdx + W, LVal + V, [I | Causes]} | xloop(I, W, V, Max, Copy, LRest)].
     
